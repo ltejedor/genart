f722c47b2d8bba5348541e55b28cdeef2d0d3909
Trevor Keith <tk@tkeith.com>
2024-02-09 19:29:50 -0500

    remove example procedure and component

--- src/app/_components/ExampleComponent.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function ExampleComponent() {
-  return <div>Hello world</div>;
-}
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import exampleProcedure from "~/server/api/procedures/exampleProcedure";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -6,9 +5,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  exampleProcedure,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
