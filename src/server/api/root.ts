375589e4773b08191f340d67d14764d85d746362
Trevor Keith <tk@tkeith.com>
2024-02-12 11:23:13 -0500

    trpc updates

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,17 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import proceduresRouter from "~/server/api/routers/proceduresRouter";
+import { createTRPCRouter, mergeRouters } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = mergeRouters(
+  proceduresRouter,
+  createTRPCRouter({
+    // additional namespaced routers go here
+  }),
+);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
